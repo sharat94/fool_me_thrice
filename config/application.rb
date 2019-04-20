35965e7715e2ae7cc58ffae8cfe8095801f6e370
sharat940 <sharatoommen94@gmail.com>
2019-04-20 23:40:25 +0530

    cors fix

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module FoolMeThrice
     config.load_defaults 5.2
     config.autoload_paths << Rails.root.join('commands')
     config.eager_load_paths << Rails.root.join('lib')
+    config.action_controller.forgery_protection_origin_check = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
