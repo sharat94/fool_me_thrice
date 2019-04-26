3807163861595890c1abb8da402ef2d957932f48
sharat940 <sharatoommen94@gmail.com>
2019-04-26 15:59:26 +0530

    dashboard data order fixed

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::DashboardController < Api::ApiController
 
   def stats
-    top_cards = Card.all.order('cards.upvotes DESC').limit(3)
+    top_cards = @current_user.cards.order('cards.upvotes DESC').limit(3)
     top_performers = find_top_performers
     render json: { top_cards: top_cards, top_performers: top_performers}
   end
@@ -9,7 +9,7 @@ class Api::V1::DashboardController < Api::ApiController
   private
 
   def find_top_performers
-    query = 'SELECT users.email, SUM(distinct(cards.upvotes)) from users INNER JOIN cards on users.id = cards.user_id GROUP BY users.email LIMIT 5'
+    query = 'SELECT users.email, SUM(distinct(cards.upvotes)) as sum from users INNER JOIN cards on users.id = cards.user_id GROUP BY users.email ORDER BY sum DESC LIMIT 5'
     result = ActiveRecord::Base.connection.execute query
     top_performers = []
     result.each do |res|
